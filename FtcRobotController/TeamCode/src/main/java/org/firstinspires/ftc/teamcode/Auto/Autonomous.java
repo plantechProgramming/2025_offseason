e9209818887639390fe5a843147292d7ab3734e1
liort <liortzuk@gmail.com>
2025-02-13 14:36:15 +0100

    1.8 added sigmod func

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Autonomous.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Autonomous.java
@@ -16,8 +16,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="idodienomous", group="Robot")
-@Disabled
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="test1", group="Robot")
 public class Autonomous extends OpMode {
 
 
@@ -32,60 +31,7 @@ public class Autonomous extends OpMode {
         Elevator2 lift = new Elevator2(armL, armR, intake_center_angle, intake_left, intake_right, intake_AR, intAR, telemetry);
         DriveTrain driveTrain = new DriveTrain(DriveBackRight, DriveBackLeft, DriveFrontRight, DriveFrontLeft, telemetry, Imu);
 
-        double botHeading = Imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-        double botDeg = Imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-
-        driveTrain.turnToGyro_plus(Math.abs(botDeg));
-        sleep(500);
-        run_to_point(driveTrain,1.5, 0, 1, 0, botHeading);
-        sleep(500);
-        driveTrain.turnToGyro_minus(-50);
-        sleep(500);
-
-        run_to_point(driveTrain,6.7, 0, -1, 0, botHeading);
-        sleep(500);
-        driveTrain.turnToGyro_plus(20);
-        run_to_point(driveTrain,0.35, 0, -1, 0, botHeading);
-
-        lift.Move_Elevator(8000);
-        sleep(100);
-
-        intake_AR.setPosition(0);
-        sleep(1000);
-
-        // new take
-        intake_AR.setPosition(0.7);
-        sleep(100);
-
-        run_to_point(driveTrain,0.3,0,0.7,0,botHeading);
-        sleep(200);
-
-
-        lift.Move_Elevator(-8000);
-        sleep(500);
-
-        driveTrain.turnToGyro_plus(40);
-        sleep(100);
-
-        run_to_point(driveTrain, 0.3,0,1,0,botHeading); // pos = 2 tiles
-        sleep(100);
-
-        lift.extend(-0.5,1);
-        sleep(500);
-
-        intake_AR.setPosition(1);
-        sleep(500);
-
-
-        run_to_point(driveTrain, 1,0,0.5,0,botHeading);
-        sleep(500);
-
-        botDeg = Imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-
-        telemetry.addData("imu: ", botDeg);
-        telemetry.update();
-
+        drive_abs_point(driveTrain, -0.5, 1);
 
 
 
