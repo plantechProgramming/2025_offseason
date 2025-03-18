ded38f68a0e12f7a23b0fa3ce68fafa13564b6eb
liort <liortzuk@gmail.com>
2025-03-18 18:08:27 +0200

    roadrunner version update 0.2.5

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/test.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/test.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.DriveTrain.DriveTrain;
+import org.firstinspires.ftc.teamcode.Elevator.Elevator;
 import org.firstinspires.ftc.teamcode.OpMode;
 
 @Autonomous
 public class test extends OpMode {
+    DriveTrain driveTrain = new DriveTrain(DriveBackRight, DriveBackLeft, DriveFrontRight, DriveFrontLeft, telemetry, Imu);
+    Elevator lift = new Elevator(armL, armR, intake_center_angle, intake_left, intake_right, intake_AR, intAR, telemetry);
+
     @Override
     protected void run() {
-
+        diagonal(driveTrain,0.2, 0.5);
     }
 
     @Override
