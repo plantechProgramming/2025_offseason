5ecc97a03bc89c6ced419e7de66897e47d0c1909
liort <liortzuk@gmail.com>
2025-05-06 17:14:31 +0300

    v 0.7.5 for help yoav

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/nextAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/nextAuto.java
@@ -6,19 +6,61 @@ import com.rowanmcalpin.nextftc.core.command.Command;
 import com.rowanmcalpin.nextftc.core.command.groups.ParallelGroup;
 import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
 
+import com.rowanmcalpin.nextftc.core.command.utility.delays.Delay;
+import com.rowanmcalpin.nextftc.core.control.controllers.Controller;
+import com.rowanmcalpin.nextftc.core.units.TimeSpan;
 import com.rowanmcalpin.nextftc.ftc.NextFTCOpMode;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.HoldPosition;
 //import org.firstinspires.ftc.teamcode.Elevator.nextIntakeAngle;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Elevator.nextLift;
 
 @Autonomous(name = "nextRoni")
 public class nextAuto extends NextFTCOpMode {
+//    Controller controller = new Controller() {
+//        @Override
+//        public double getTarget() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public void setTarget(double v) {
+//
+//        }
+//
+//        @Override
+//        public double getSetPointTolerance() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public void setSetPointTolerance(double v) {
+//
+//        }
+//
+//        @Override
+//        public double calculate(double v) {
+//            return 0;
+//        }
+//
+//        @Override
+//        public void reset() {
+//
+//        }
+//
+//        @Override
+//        public boolean atTarget(double v) {
+//            return false;
+//        }
+//    };
+
     public nextAuto() {
         super( nextLift.INSTANCE);
     }
-    public Command firstRoutine() {
+    public Command firstRoutine()  {
         return new SequentialGroup(
-                nextLift.INSTANCE.toHigh(),
-                nextLift.INSTANCE.holdForTime(500)
+                nextLift.INSTANCE.toHigh()
+
         );
     }
 
--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator/nextLift.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator/nextLift.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Elevator;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.rowanmcalpin.nextftc.core.command.Command;
 import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
 import com.rowanmcalpin.nextftc.core.command.utility.delays.Delay;
@@ -10,15 +11,22 @@ import com.rowanmcalpin.nextftc.core.units.TimeSpan;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.HoldPosition;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
+import com.rowanmcalpin.nextftc.core.command.CommandManager;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import org.firstinspires.ftc.teamcode.Elevator.Elevator;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+@Config
 public class nextLift extends Subsystem {
 
     public static final nextLift INSTANCE = new nextLift();
     private nextLift() { }
-    public PIDFController controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0));
+    public static double kP_next = 0.15;
+    public static double kI_next = 0.05;
+    public static double kD_next = 0.05;
+    public PIDFController controller = new PIDFController(kP_next, kI_next, kD_next, new StaticFeedforward(0));
     public MotorEx motor;
-    public String name = "EH";
 
 
 
@@ -31,13 +39,23 @@ public class nextLift extends Subsystem {
     }
 
     public Command toHigh() {
-
         return new RunToPosition(motor, // MOTOR TO MOVE
-                2800, // TARGET POSITION, IN TICKS
+                2950, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
-
     }
+//    public  Command  holdPos(long time){
+//
+////            double count = 0;
+////            if(count <= 1) {
+////                count += 1;
+////
+////            }count-=1;
+////
+////        return null;
+//    }
+
+
 
 
 //    public Command holdForTime(long x) {
@@ -51,8 +69,9 @@ public class nextLift extends Subsystem {
 
     @Override
     public void initialize() {
-        motor = new MotorEx(name);
+        motor = new MotorEx("EH");
         motor.reverse();
     }
+
 
 }
